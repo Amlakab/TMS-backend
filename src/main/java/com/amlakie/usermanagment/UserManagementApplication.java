e60f20f5ce86f32d6242721e77cf6e479fbdff51
Amlakab <amlakieabebaw96@gmail.com>
2025-06-19 09:14:12 +0300

    maintainace implementation added

--- src/main/java/com/amlakie/usermanagment/UserManagementApplication.java
+++ src/main/java/com/amlakie/usermanagment/UserManagementApplication.java
@@ -1,7 +1,7 @@
 package com.amlakie.usermanagment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-=
+
 @SpringBootApplication
  public class  UserManagementApplication {
 	public static void main(String[] args) {
